f4d737bd74e38f868da62f006da02b2bb8fe6523
kajisan0415 <agonagakajisan0415@gmail.com>
2021-05-05 08:57:27 +0000

    Chapter7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_053217) do
+ActiveRecord::Schema.define(version: 2021_05_05_081555) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -53,6 +53,11 @@ ActiveRecord::Schema.define(version: 2021_05_02_053217) do
     t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
+    t.string "address_building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
